598ccaa81bb99aab55e3c79a776356c2059ce64b
Hiercy <volk10051@gmail.com>
2020-06-14 16:31:13 +0300

    New methods for repository.

--- src/main/java/com/mike/reddit/repository/CommentRepository.java
+++ src/main/java/com/mike/reddit/repository/CommentRepository.java
@@ -1,9 +1,16 @@
 package com.mike.reddit.repository;
 
 import com.mike.reddit.model.Comment;
+import com.mike.reddit.model.Customer;
+import com.mike.reddit.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByCustomer(Customer customer);
+
+    List<Comment> findAllByPost(Post post);
 }
