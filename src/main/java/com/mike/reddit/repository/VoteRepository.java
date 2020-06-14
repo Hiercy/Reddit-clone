24ffa70ae374ce30363726d94916740c9e7d8aed
Hiercy <volk10051@gmail.com>
2020-06-14 23:33:27 +0300

    New method for find last user vote into VoteRepository.

--- src/main/java/com/mike/reddit/repository/VoteRepository.java
+++ src/main/java/com/mike/reddit/repository/VoteRepository.java
@@ -1,9 +1,15 @@
 package com.mike.reddit.repository;
 
+import com.mike.reddit.model.Customer;
+import com.mike.reddit.model.Post;
 import com.mike.reddit.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndCustomerOrderByVoteIdDesc(Post post, Customer customer);
 }
+
