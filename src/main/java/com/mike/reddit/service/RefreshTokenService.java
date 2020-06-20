d2381b8f171fc1ae1cc3eff1502d311c3f1fee79
Hiercy <volk10051@gmail.com>
2020-06-20 13:02:13 +0300

    Service, Repository and Dto for RefreshToken

---
+++ src/main/java/com/mike/reddit/service/RefreshTokenService.java
@@ -0,0 +1,40 @@
+package com.mike.reddit.service;
+
+import com.mike.reddit.exceptions.SpringRedditException;
+import com.mike.reddit.model.RefreshToken;
+import com.mike.reddit.repository.RefreshTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public void validateRefreshToken(String token) {
+        refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new SpringRedditException("Invalid refresh Token!"));
+    }
+
+    public void deleteRefresgToken(String token) {
+        refreshTokenRepository.deleteByToken(token);
+    }
+}
